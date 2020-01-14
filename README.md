004305379a19df81872bacf6ed683a8bbd91a406
Drozerah <drozerah@gmail.com>
2020-01-14 09:58:32 +0100

    feat: setup express server

---
+++ README.md
@@ -0,0 +1,5 @@
+# Node.js/Express/MongoDB application
+
+> #TODO
+
+- write a better README.md
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Hello!')
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server listening on http://localhost:${port}/`)
+})
